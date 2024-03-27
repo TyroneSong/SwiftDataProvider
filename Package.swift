589cef37a694ddca8e032fea7b064c357fdf0556
Tyrone <songpu_work@163.com>
2024-03-27 09:18:35 +0800

    add article source

--- Package.swift
+++ Package.swift
@@ -1,6 +1,8 @@
 // swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+// Source: https://fatbobman.com/zh/posts/practical-swiftdata-building-swiftui-applications-with-modern-approaches/
+
 import PackageDescription
 
 let package = Package(
